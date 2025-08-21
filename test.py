e24919122f09caca5465a1f6415c9754284d728f
Allen <129114411+allenpotts13@users.noreply.github.com>
2025-08-21 10:17:06 -0500

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-import duckdb
-con = duckdb.connect('/workspaces/nyc_hiring_audit_project/duckdb/nyc_hiring.duckdb')
-print(con.execute("SELECT * FROM information_schema.tables").fetchdf())  # List all tables
